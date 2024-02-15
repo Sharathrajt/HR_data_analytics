fd31004bbfb4a7631f411ac25a3f106e2eedf33d
Sharathrajt <157151391+Sharathrajt@users.noreply.github.com>
2024-02-15 19:17:54 +0530

    Add files via upload

---
+++ HR_SQL.sql
@@ -0,0 +1,140 @@
+CREATE DATABASE hr_analytics
+
+USE hr_analytics;
+
+SELECT * FROM hr;
+
+		-- DATA CLEANING--
+
+ALTER TABLE hr
+CHANGE COLUMN ï»¿id emp_id VARCHAR(20) NULL;
+
+SET sql_safe_updates = 0;
+
+UPDATE hr
+SET birthdate = CASE
+		WHEN birthdate LIKE '%/%' THEN date_format(str_to_date(birthdate,'%m/%d/%Y'),'%Y-%m-%d')
+        WHEN birthdate LIKE '%-%' THEN date_format(str_to_date(birthdate,'%m-%d-%Y'),'%Y-%m-%d')
+        ELSE NULL
+		END;
+        
+ALTER TABLE hr
+MODIFY COLUMN birthdate DATE;
+
+UPDATE hr
+SET hire_date = CASE
+		WHEN hire_date LIKE '%/%' THEN date_format(str_to_date(hire_date,'%m/%d/%Y'),'%Y-%m-%d')
+        WHEN hire_date LIKE '%-%' THEN date_format(str_to_date(hire_date,'%m-%d-%Y'),'%Y-%m-%d')
+        ELSE NULL
+		END;
+        
+ALTER TABLE hr
+MODIFY COLUMN hire_date DATE;
+
+UPDATE hr
+SET termdate = date(str_to_date(termdate, '%Y-%m-%d %H:%i:%s UTC'))
+WHERE termdate IS NOT NULL;
+
+UPDATE hr
+SET termdate = NULL
+WHERE termdate = '';
+
+ALTER TABLE hr
+MODIFY COLUMN termdate DATE;
+
+		-- ADDING AGE COLUMN --
+
+ALTER TABLE hr
+ADD column age INT;
+
+UPDATE hr
+SET age = timestampdiff(YEAR,birthdate,curdate())
+
+		-- 1. What is the gender breakdown of employees in the company? -- 
+        
+SELECT gender, COUNT(*) AS emp_count 
+FROM hr
+WHERE termdate IS NULL
+GROUP BY gender;
+
+		-- 2. What is the race breakdown of employees in the company? --
+        
+SELECT race , COUNT(*) AS emp_count
+FROm hr
+WHERE termdate IS NULL
+GROUP BY race
+
+		-- 3. What is the age distribution of employees in the company? --
+SELECT 
+	CASE
+		WHEN age>=18 AND age<=24 THEN '18-24'
+        WHEN age>=25 AND age<=34 THEN '25-34'
+        WHEN age>=35 AND age<=44 THEN '35-44'
+        WHEN age>=45 AND age<=54 THEN '45-54'
+        WHEN age>=55 AND age<=64 THEN '55-64'
+        ELSE '65+'
+	END AS age_group,
+    COUNT(*) AS count
+    FROM hr
+    WHERE termdate IS NULL
+    GROUP BY age_group
+    ORDER BY age_group;
+
+		-- 4. How many employees work at headquarters vs remote? --
+        
+SELECT location,COUNT(*) AS emp_count
+FROm hr
+WHERE termdate IS NULL
+GROUP BY location;
+
+		-- 5. What is the average duration of terminated employee? --
+        
+SELECT ROUND(AVG(year(termdate) - year(hire_date)),0) AS length_of_emp
+FROM hr
+WHERE termdate IS NOT NULL AND termdate < curdate();
+
+		-- 6. How does the gender distribution vary acorss department? --
+
+SELECT department,gender,COUNT(*) AS emp_count
+FROM hr
+WHERE termdate IS NULL
+GROUP BY department,gender
+ORDER BY department,gender
+
+		-- 7. What is the distribution of jobtitles acorss the company? --
+        
+SELECT jobtitle, COUNT(*) AS emp_count
+FROM hr
+WHERE termdate IS NULL
+GROUP BY jobtitle
+
+		-- 8. Which dept has the highest termination rate? -- 
+
+SELECT department,
+		COUNT(*) AS total_count,
+        COUNT(CASE
+				WHEN termdate IS NOT NULL AND termdate <= curdate() THEN 1 
+				END) AS terminated_count,
+		ROUND((COUNT(CASE
+					WHEN termdate IS NOT NULL AND termdate <= curdate() THEN 1 
+                    END)/COUNT(*))*100,2) AS termination_rate
+		FROM hr
+        GROUP BY department
+        ORDER BY termination_rate DESC
+        
+		-- 9. What is the distribution of employees across location_state? --
+        
+SELECT location_state, COUNT(*) AS emp_count
+FROm hr
+WHERE termdate IS NULL
+GROUP BY location_state
+
+		-- 11. What is the tenure distribution for each dept.
+        
+SELECT department, round(avg(timestampdiff(year,hire_date,termdate)),0) as avg_tenure
+FROM hr
+WHERE termdate IS NOT NULL AND termdate <= curdate()
+GROUP BY department
+
+
+
